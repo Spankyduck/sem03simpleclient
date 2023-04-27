40d966cff1764a4469c50710a18f5e166d7b8c6b
root <sejessen4@gmail.com>
2023-04-27 14:16:42 +0000

    lolol

---
+++ go.mod
@@ -0,0 +1,8 @@
+module tcp_client.go
+
+go 1.18
+
+require (
+	github.com/Spankyduck/funtemps v0.0.0-20230220150230-57334d79374a // indirect
+	github.com/Spankyduck/is105sem03 v0.0.0-20230419090342-bbabb2540b34 // indirect
+)
--- tcp_client.go
+++ tcp_client.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+
 	"net"
 	"log"
 	"os"
+        "github.com/Spankyduck/is105sem03/mycrypt"
 )
 
+
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.2:12345")
 	if err != nil {
 		log.Fatal(err)
-	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
+        }
+        log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+        kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("kryptert melding: ", string(kryptertMelding))
+        
+
+ 	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +28,9 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+
+        dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, (len(mycrypt.ALF_SEM03) -4))
+        log.Printf("reply from proxy: %s", string(dekryptertMelding))
+
 }
